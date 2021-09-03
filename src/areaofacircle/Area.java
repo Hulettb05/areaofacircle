7b3f6588f52d5652df59cfc3eec5dac444b7b338
Brian Huett <bbhulett@gmail.com>
2021-09-02 19:46:06 -0500

    removed notes that were giving minor error

--- src/areaofacircle/Area.java
+++ src/areaofacircle/Area.java
@@ -8,8 +8,7 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double radius = input.nextDouble();
-//        Double area = 3.14 * radius * radius;
-//        Double area = Circle.getArea(radius);
+
         System.out.println("The area of a circe of radius " + radius + " is: " + Circle.getArea(radius));
     }
 }
